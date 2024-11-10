8f569aefde4277359317419252c1d1ee84c62a53
Gerhard <gerhardrvv@gmail.com>
2024-11-10 12:47:46 -0700

    feat: implement web socket functionality
    - Web socket functionality added to wsClient.html
    - Set artifitial 1 sec delay to verify the socket functionality

---
+++ taskhub/src/main/kotlin/com/example/plugins/Sockets.kt
@@ -0,0 +1,46 @@
+package com.example.plugins
+
+import com.example.model.Task
+import com.example.model.TaskRepository
+import io.ktor.server.application.Application
+import io.ktor.server.routing.routing
+import io.ktor.server.websocket.DefaultWebSocketServerSession
+import io.ktor.server.websocket.WebSocketServerSession
+import io.ktor.server.websocket.receiveDeserialized
+import io.ktor.server.websocket.sendSerialized
+import io.ktor.server.websocket.webSocket
+import io.ktor.websocket.CloseReason
+import io.ktor.websocket.close
+import kotlinx.coroutines.delay
+import java.util.Collections
+
+fun Application.configureSockets() {
+    routing {
+        val sessions = Collections.synchronizedList<WebSocketServerSession>(ArrayList())
+
+        webSocket("/tasks") {
+            sendAllTasks()
+            close(CloseReason(CloseReason.Codes.NORMAL, "All done"))
+        }
+
+        webSocket("/tasks2") {
+            sessions.add(this)
+            sendAllTasks()
+
+            while(true) {
+                val newTask = receiveDeserialized<Task>()
+                TaskRepository.addTask(newTask)
+                for(session in sessions) {
+                    session.sendSerialized(newTask)
+                }
+            }
+        }
+    }
+}
+
+private suspend fun DefaultWebSocketServerSession.sendAllTasks() {
+    for(task in TaskRepository.allTasks()) {
+        sendSerialized(task)
+        delay(1000)
+    }
+}
